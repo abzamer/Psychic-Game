c350f86555cae5c10eebbcca1f7aa8c98b0526f6
Angelica Zamer <angelica.zamer@gmail.com>
2019-09-06 22:14:39 -0500

    added notes in js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+//refer to activity 34. for this first step
+//this will track what key the user pressed
+
+var userChoice = event.key.toLowerCase();
+console.log(userChoice);
+
+
+//checck order: this will compare the userChoice to computerChoice & If userChoice is wrong, subtract one from guessesLeft
+
+
+
+
+//this will add the letterPressed to "guesses so far"
+
+
+
+//I need to make the computer pick a random letter
+
+
+
+
+//if guessesLeft === 0, user loses the game
+
+
+
+//restarts the game & adds 1 to losses
+
+
+
+
+//If userChoice matches computerChoice, Restart game & add 1 to wins
+
